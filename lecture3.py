c0ef3ea0e42423c9317cfccc0f4b44f19b1c2248
Ubuntu <ntu-admin@ML-RefVm-708080.m2urcaraerduxjtolbtfeyione.zx.internal.cloudapp.net>
2022-09-01 14:52:45 +0100

    update

---
+++ lecture3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+mat=[[1,2,3],[4,5,6]]
+print(mat[0][0],"-->mat[0][0]")
+print(mat[0][1],"-->mat[0][1]")
+print(mat[0][2],"-->mat[0][2]")
+print(mat[1][0],"-->mat[1][0]")
+print(mat[1][1],"-->mat[1][1]")
+print(mat[1][2],"-->mat[1][2]")
+mat[1][1]=8
